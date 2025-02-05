f0a028cb9e2b7342431b253f3cd73b1cc9465c52
Jose <joseortizv.nyc@gmail.com>
2025-02-04 22:20:27 -0500

    Classwork2.py UPDATE

--- Classwork/Classwork2.py
+++ Classwork/Classwork2.py
@@ -1,8 +1,21 @@
 """
 Name: Jose Miguel Ortiz
 Email: jose.ortiz60@lagcc.cuny.edu
-Pod:
-Date:
-Description:
+Pod: 5th right
+Date: 02/04/2025
 
 """
+
+import pandas as pd
+
+if __name__ == "__main__":
+
+    input_file = input("Enter an input file name: ")
+    output_file = input("Enter an output file name:  ")
+
+    df = pd.read_csv(input_file)
+
+    test_results = df[(df['Grade'] == 3) and (df['Year'] == 2019)]
+    test_results.to_csv(output_file, index=False)
+
+    print(f"Filtered data has been saved to {output_file}")
