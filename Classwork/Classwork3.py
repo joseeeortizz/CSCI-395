1fadb1f94d58ad019785f17a270cb92d9ff9400f
Jose <joseortizv.nyc@gmail.com>
2025-02-06 18:40:00 -0500

    Classwork3.py UPDATE

--- Classwork/Classwork3.py
+++ Classwork/Classwork3.py
@@ -1,8 +1,22 @@
 """
 Name: Jose Miguel Ortiz
 Email: jose.ortiz60@lagcc.cuny.edu
-Pod:
+Pod: 1st right
 Date: 02/06/2025
 
 """
 
+# complete the lambda function
+cube = lambda x: x ** 3
+
+def fibonacci(n):
+
+    fib = [0, 1]
+    for i in range(2, n):
+        fib.append(fib[i - 1] + fib[i - 2])
+    return fib[:n+1]
+
+if __name__ == '__main__':
+    print("Enter the number of terms you want in the fibonacci series: ")
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
