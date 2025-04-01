bb2ec9d23df53535512f9377021ad1cbad9d5a19
Jose <joseortizv.nyc@gmail.com>
2025-04-01 16:46:53 -0400

    April 1st UPDATE

--- Classwork/Classwork13.py
+++ Classwork/Classwork13.py
@@ -2,7 +2,8 @@
 Name: Jose Miguel Ortiz
 Email: jose.ortiz60@lagcc.cuny.edu
 Pod:
-Date: 02/25/2025
+Date: 02/27/2025
 
 """
 
+if __name__ == '__main__':
--- Classwork/Classwork9.py
+++
@@ -1,7 +0,0 @@
-"""
-Name: Jose Miguel Ortiz
-Email: jose.ortiz60@lagcc.cuny.edu
-Pod:
-Date: 03//2025
-
-"""
